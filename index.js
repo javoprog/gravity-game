71d89b1a50c2bfbe6f57dddb18c84103d7417aa1
JavoProg <progjavo@gmail.com>
2026-07-03 09:50:45 +0500

    setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener("resize", (event) => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
